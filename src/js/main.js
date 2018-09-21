ae81e315eb0d6731a8c111f27e089d2dfab9aca2
Brad Lannon <BLannon@Clearwater.ca>
2018-09-21 08:05:23 -0300

    Added lightbox to image gallery

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,8 @@
+$(document).on('click', '[data-toggle="lightbox"]', function(event) {
+    event.preventDefault();
+    $(this).ekkoLightbox();
+});
+
 $("#myWoof").click(function(){
     fetchImage();
 }); 
@@ -9,9 +14,9 @@ function fetchImage() {
 }
 
 function displayImage(imgurl) {
-    var format = imgurl.substr(imgurl.length - 3)
+    var format = imgurl.substr(imgurl.length - 3);
     if (format == 'jpg' || format == 'JPG' || format == 'gif' || format == 'png') {
-        $('#pupperGallery').prepend('<div class="col-lg-3 col-md-4 col-xs-6"><a href="' + imgurl + '" class="d-block mb-4 h-100">  <img class="img-thumbnail" id="imgGallery" src="' + imgurl + '" alt="Puppy Picture"></a></div>')
+        $('#pupperGallery').prepend('<div class="col-lg-3 col-md-4 col-xs-6"><a href="' + imgurl + '" data-toggle="lightbox" class="d-block mb-4 h-100">  <img class="img-thumbnail" id="imgGallery" src="' + imgurl + '" alt="Puppy Picture"></a></div>');
     } else {
         //console.log(format + " is an incompatible format"); // video files will not display as image
         fetchImage(); // fetch for next potential image
